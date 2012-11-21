5c5c5a56f3c5fd6c5951827e554be3fbfa10e3f5
Akira Takahashi <faithandbrave@gmail.com>
2012-11-21 16:44:42 +0900

    add tagged_int class for strong typedef. add stream operator to tagged_real.

---
+++ libs/strong_typedef/tagged_int_example.cpp
@@ -0,0 +1,30 @@
+// Copyright Akira Takahashi 2012
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <iostream>
+#include <ctime>
+#include <shand/strong_typedef/tagged_int.hpp>
+
+struct count_tag {};
+struct seconds_tag {};
+
+int main()
+{
+	shand::tagged_int<std::size_t, count_tag> count = 0;
+	shand::tagged_int<std::time_t, seconds_tag> seconds = std::time(0);
+
+    // 異なる型(タグ)間での暗黙変換はできない
+//	count = seconds; // コンパイルエラー！型が違う
+
+	std::cout << count << std::endl;
+	std::cout << seconds << std::endl;
+}
+
+/*
+possible output:
+0
+1353483810
+*/
+
---
+++ shand/strong_typedef/tagged_int.hpp
@@ -0,0 +1,67 @@
+#ifndef SHAND_TAGGED_INT_INCLUDE
+#define SHAND_TAGGED_INT_INCLUDE
+
+// strong typedef for integer types.
+//
+// Copyright Akira Takahashi 2012
+// Use, modification and distribution is subject to the Boost Software License,
+// Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#include <iosfwd>
+#include <boost/type_traits/is_integral.hpp>
+#include <boost/static_assert.hpp>
+#include <boost/operators.hpp>
+
+namespace shand {
+
+template <class Integer, class Tag> // Tag is phantom type
+class tagged_int :
+        private boost::operators<tagged_int<Integer, Tag> > {
+
+    BOOST_STATIC_ASSERT(boost::is_integral<Integer>::value);
+
+    Integer value_;
+public:
+    typedef Integer value_type;
+
+    tagged_int(Integer value = 0) : value_(value) {}
+
+    Integer& get() { return value_; }
+    const Integer& get() const { return value_; }
+
+    // operators
+    bool operator<(const tagged_int& x) const { return value_ < x.value_; }
+    bool operator==(const tagged_int& x) const { return value_ == x.value_; }
+    tagged_int& operator+=(const tagged_int& x) { value_ += x.value_; return *this; }
+    tagged_int& operator-=(const tagged_int& x) { value_ -= x.value_; return *this; }
+    tagged_int& operator*=(const tagged_int& x) { value_ *= x.value_; return *this; }
+    tagged_int& operator/=(const tagged_int& x) { value_ /= x.value_; return *this; }
+    tagged_int& operator%=(const tagged_int& x) { value_ %= x.value_; return *this; }
+
+    tagged_int& operator&=(const tagged_int& x) { value_ &= x.value_; return *this; }
+    tagged_int& operator|=(const tagged_int& x) { value_ |= x.value_; return *this; }
+    tagged_int& operator^=(const tagged_int& x) { value_ ^= x.value_; return *this; }
+
+    tagged_int& operator++() { ++value_; return *this; }
+    tagged_int& operator--() { --value_; return *this; }
+};
+
+template <class CharT, class Traits, class Integer, class Tag>
+std::basic_ostream<CharT, Traits>&
+    operator<<(std::basic_ostream<CharT, Traits>& os, const tagged_int<Integer, Tag>& x)
+{
+    return os << x.get();
+}
+
+template <class CharT, class Traits, class Integer, class Tag>
+std::basic_istream<CharT, Traits>&
+    operator<<(std::basic_istream<CharT, Traits>& is, const tagged_int<Integer, Tag>& x)
+{
+    return is >> x.get();
+}
+
+} // namespace shand
+
+#endif
+
