86c129095e286164fb678d64e115786d51712660
Akira Takahashi <faithandbrave@gmail.com>
2013-04-18 15:40:53 +0900

    fix tab

--- libs/errorable/example.cpp
+++ libs/errorable/example.cpp
@@ -11,33 +11,33 @@
 
 int main()
 {
-	// 初期状態はエラー
+    // 初期状態はエラー
     {
-		shand::errorable<int> x;
-		BOOST_ASSERT(x.is_error());
+        shand::errorable<int> x;
+        BOOST_ASSERT(x.is_error());
     }
 
-	// エラーが発生しているケース
-	// エラー理由を格納する
-	// デフォルトのエラー型はstd::string
+    // エラーが発生しているケース
+    // エラー理由を格納する
+    // デフォルトのエラー型はstd::string
     {
-		shand::errorable<int> value = shand::error("invalid argument");
+        shand::errorable<int> value = shand::error("invalid argument");
         if (boost::optional<std::string> err = value.get_error()) {
             std::cout << err.get() << std::endl;
         }
     }
 
-	// 正常な値が入っているケース
+    // 正常な値が入っているケース
     {
-		shand::errorable<int> value = 3;
+        shand::errorable<int> value = 3;
         if (boost::optional<int> x = value.get_value()) {
             std::cout << x.get() << std::endl;
         }
     }
 
-	// 値が正常かエラーかによって、関数を呼び分ける
+    // 値が正常かエラーかによって、関数を呼び分ける
     {
-		shand::errorable<int> value = shand::error("invalid argument");
+        shand::errorable<int> value = shand::error("invalid argument");
         value.value_if([](int x) {
             std::cout << x << std::endl;
         })
@@ -46,12 +46,12 @@ int main()
         });
     }
 
-	// 第2テンプレート引数で、エラー型をカスタマイズ可能
-	{
-		shand::errorable<int, std::errc> value = shand::error(std::errc::invalid_argument);
-		if (boost::optional<std::errc> err = value.get_error()) {
-			std::cout << std::make_error_code(err.get()).message() << std::endl;
-		}
-	}
+    // 第2テンプレート引数で、エラー型をカスタマイズ可能
+    {
+        shand::errorable<int, std::errc> value = shand::error(std::errc::invalid_argument);
+        if (boost::optional<std::errc> err = value.get_error()) {
+            std::cout << std::make_error_code(err.get()).message() << std::endl;
+        }
+    }
 }
 
