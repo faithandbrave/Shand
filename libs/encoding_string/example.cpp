5b6b84228d7bfc2f6d56a2a8f23b69b282a2c68f
Akira Takahashi <faithandbrave@gmail.com>
2013-12-19 22:11:08 +0900

    add output stream example.

--- libs/encoding_string/example.cpp
+++ libs/encoding_string/example.cpp
@@ -6,7 +6,10 @@ int main()
     using namespace shand;
     encoding_string<encoding::utf8> s = u8"あいうえお";
 
+    // 文字数を取得
     std::cout << s.codeunit_size() << std::endl; // 5
+
+    // i番目の文字を取得
     std::cout << s.codeunit_at(2) << std::endl; // u8"う"
 
     // 1文字ずつ出力
@@ -18,5 +21,9 @@ int main()
     // う
     // え
     // お
+    
+    // システムの文字コードで出力
+    // WindowsではCP932で出力する
+    std::cout << s << std::endl;
 }
 
