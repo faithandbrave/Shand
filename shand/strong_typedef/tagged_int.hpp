e9c9bbec939e9a2a8826015c7e724c2f7b9a5559
Akira Takahashi <faithandbrave@gmail.com>
2015-03-27 12:03:19 +0900

    add noexcept to tagged_int/tagged_real.

--- shand/strong_typedef/tagged_int.hpp
+++ shand/strong_typedef/tagged_int.hpp
@@ -3,16 +3,25 @@
 
 // strong typedef for integer types.
 //
-// Copyright Akira Takahashi 2012-2014.
+// Copyright Akira Takahashi 2012-2015.
 // Use, modification and distribution is subject to the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/type_traits/is_integral.hpp>
+#include <boost/type_traits/has_nothrow_copy.hpp>
 #include <boost/static_assert.hpp>
 #include <boost/operators.hpp>
 #include <boost/functional/hash.hpp>
 #include <functional>
+
+#if defined(BOOST_NO_CXX11_NOEXCEPT)
+    #define SHAND_NOEXCEPT_COND(cond) noexcept(cond)
+    #define SHAND_NOEXCEPT_IF(expr) noexcept(noexcept(expr))
+#else
+    #define SHAND_NOEXCEPT_COND(cond)
+    #define SHAND_NOEXCEPT_IF(expr)
+#endif
 
 namespace shand {
 
@@ -26,31 +35,46 @@ class tagged_int :
 public:
     typedef Integer value_type;
 
-    explicit tagged_int(Integer value = 0) : value_(value) {}
+    explicit tagged_int(Integer value = 0)
+        SHAND_NOEXCEPT_COND(boost::has_nothrow_copy_constructor<Integer>::value)
+        : value_(value) {}
 
-    Integer& get() { return value_; }
-    const Integer& get() const { return value_; }
+    Integer& get() BOOST_NOEXCEPT { return value_; }
+    const Integer& get() const BOOST_NOEXCEPT { return value_; }
 
     // operators
-    bool operator<(const tagged_int& x) const { return value_ < x.value_; }
-    bool operator==(const tagged_int& x) const { return value_ == x.value_; }
-    tagged_int& operator+=(const tagged_int& x) { value_ += x.value_; return *this; }
-    tagged_int& operator-=(const tagged_int& x) { value_ -= x.value_; return *this; }
-    tagged_int& operator*=(const tagged_int& x) { value_ *= x.value_; return *this; }
-    tagged_int& operator/=(const tagged_int& x) { value_ /= x.value_; return *this; }
-    tagged_int& operator%=(const tagged_int& x) { value_ %= x.value_; return *this; }
+    bool operator<(const tagged_int& x) const SHAND_NOEXCEPT_IF(value_ < x.value_)
+        { return value_ < x.value_; }
+    bool operator==(const tagged_int& x) const SHAND_NOEXCEPT_IF(value_ == x.value_)
+        { return value_ == x.value_; }
 
-    tagged_int& operator&=(const tagged_int& x) { value_ &= x.value_; return *this; }
-    tagged_int& operator|=(const tagged_int& x) { value_ |= x.value_; return *this; }
-    tagged_int& operator^=(const tagged_int& x) { value_ ^= x.value_; return *this; }
+    tagged_int& operator+=(const tagged_int& x) SHAND_NOEXCEPT_IF(value_ += x.value_)
+        { value_ += x.value_; return *this; }
+    tagged_int& operator-=(const tagged_int& x) SHAND_NOEXCEPT_IF(value_ -= x.value_)
+        { value_ -= x.value_; return *this; }
+    tagged_int& operator*=(const tagged_int& x) SHAND_NOEXCEPT_IF(value_ *= x.value_)
+        { value_ *= x.value_; return *this; }
+    tagged_int& operator/=(const tagged_int& x) SHAND_NOEXCEPT_IF(value_ /= x.value_)
+        { value_ /= x.value_; return *this; }
+    tagged_int& operator%=(const tagged_int& x) SHAND_NOEXCEPT_IF(value_ %= x.value_)
+        { value_ %= x.value_; return *this; }
 
-    tagged_int& operator++() { ++value_; return *this; }
-    tagged_int& operator--() { --value_; return *this; }
+    tagged_int& operator&=(const tagged_int& x) SHAND_NOEXCEPT_IF(value_ &= x.value_)
+        { value_ &= x.value_; return *this; }
+    tagged_int& operator|=(const tagged_int& x) SHAND_NOEXCEPT_IF(value_ |= x.value_)
+        { value_ |= x.value_; return *this; }
+    tagged_int& operator^=(const tagged_int& x) SHAND_NOEXCEPT_IF(value_ ^= x.value_)
+        { value_ ^= x.value_; return *this; }
+
+    tagged_int& operator++() SHAND_NOEXCEPT_IF(++value_)
+        { ++value_; return *this; }
+    tagged_int& operator--() SHAND_NOEXCEPT_IF(--value_)
+        { --value_; return *this; }
 };
 
 // Boost hash support
 template <class Integer, class Tag>
-std::size_t hash_value(const tagged_int<Integer, Tag>& x)
+std::size_t hash_value(const tagged_int<Integer, Tag>& x) SHAND_NOEXCEPT_IF(boost::hash_value(x.get()))
 {
     return boost::hash_value(x.get());
 }
@@ -75,6 +99,8 @@ struct hash<shand::tagged_int<Integer, Tag> > {
 } // namespace std
 
 #endif
+
+#undef SHAND_NOEXEPT_IF
 
 #endif
 
--- shand/strong_typedef/tagged_real.hpp
+++ shand/strong_typedef/tagged_real.hpp
@@ -3,15 +3,24 @@
 
 // strong typedef for floating point types.
 //
-// Copyright Akira Takahashi 2012
+// Copyright Akira Takahashi 2012-2015.
 // Use, modification and distribution is subject to the Boost Software License,
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/type_traits/is_floating_point.hpp>
+#include <boost/type_traits/has_nothrow_copy.hpp>
 #include <boost/static_assert.hpp>
 #include <boost/operators.hpp>
 #include <boost/geometry/util/math.hpp>
+
+#if defined(BOOST_NO_CXX11_NOEXCEPT)
+    #define SHAND_NOEXCEPT_COND(cond) noexcept(cond)
+    #define SHAND_NOEXCEPT_IF(expr) noexcept(noexcept(expr))
+#else
+    #define SHAND_NOEXCEPT_COND(cond)
+    #define SHAND_NOEXCEPT_IF(expr)
+#endif
 
 namespace shand {
 
@@ -25,19 +34,26 @@ class tagged_real :
 public:
     typedef FloatingPoint value_type;
 
-    explicit tagged_real(FloatingPoint value = 0) : value_(value) {}
+    explicit tagged_real(FloatingPoint value = 0)
+        SHAND_NOEXCEPT_COND(boost::has_nothrow_copy_constructor<FloatingPoint>::value)
+        : value_(value) {}
 
-    FloatingPoint& get() { return value_; }
-    const FloatingPoint& get() const { return value_; }
+    FloatingPoint& get() BOOST_NOEXCEPT { return value_; }
+    const FloatingPoint& get() const BOOST_NOEXCEPT { return value_; }
 
     // operators
-    bool operator<(const tagged_real& x) const { return value_ < x.value_; }
-    tagged_real& operator+=(const tagged_real& x) { value_ += x.value_; return *this; }
-    tagged_real& operator-=(const tagged_real& x) { value_ -= x.value_; return *this; }
-    tagged_real& operator*=(const tagged_real& x) { value_ *= x.value_; return *this; }
-    tagged_real& operator/=(const tagged_real& x) { value_ /= x.value_; return *this; }
+    bool operator<(const tagged_real& x) const SHAND_NOEXCEPT_IF(value_ < x.value_)
+        { return value_ < x.value_; }
+    tagged_real& operator+=(const tagged_real& x) SHAND_NOEXCEPT_IF(value_ += x.value_)
+        { value_ += x.value_; return *this; }
+    tagged_real& operator-=(const tagged_real& x) SHAND_NOEXCEPT_IF(value_ -= x.value_)
+        { value_ -= x.value_; return *this; }
+    tagged_real& operator*=(const tagged_real& x) SHAND_NOEXCEPT_IF(value_ *= x.value_)
+        { value_ *= x.value_; return *this; }
+    tagged_real& operator/=(const tagged_real& x) SHAND_NOEXCEPT_IF(value_ /= x.value_)
+        { value_ /= x.value_; return *this; }
 
-    bool equals(const tagged_real& x) const
+    bool equals(const tagged_real& x) const SHAND_NOEXCEPT_IF(boost::geometry::math::equals(value_, x.value_))
     {
         return boost::geometry::math::equals(value_, x.value_);
     }
